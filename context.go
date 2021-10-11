4971b71590d0cb140a18d2f81996e170cb45d75a
ghosind <ghosind@gmail.com>
2021-10-11 22:31:29 +0800

    Update context methods

--- context.go
+++ context.go
@@ -53,10 +53,13 @@ func (ctx *Context) finalize() {
 	ctx.app.pool.Put(ctx)
 }
 
+// writeResponse writes data from context to the response.
 func (ctx *Context) writeResponse(rw http.ResponseWriter) {
 	ctx.Response.write(rw)
 }
 
+// send writes the response data to the body buffer, sets the contentType and the status code
+// if it's set.
 func (ctx *Context) send(data []byte, contentType string, statusCode ...int) error {
 	if len(statusCode) >= 1 {
 		err := ctx.Response.SetStatusCode(statusCode[0])
@@ -98,56 +101,75 @@ func (ctx *Context) Use(handlers ...HandlerFunc) {
 	ctx.handlers = append(ctx.handlers, handlers...)
 }
 
+// Log call the app logger with the given format and args.
 func (ctx *Context) Log(fmt string, args ...interface{}) {
 	ctx.app.log(fmt, args...)
 }
 
+// Get retrieves the value of the given key from the context state.
 func (ctx *Context) Get(key string) (interface{}, bool) {
 	val, ok := ctx.state[key]
 
 	return val, ok
 }
 
+// Set sets the value of the given key to the context state.
 func (ctx *Context) Set(key string, val interface{}) {
 	ctx.state[key] = val
 }
 
+// Cookie returns the named cookie provided in the request.
 func (ctx *Context) Cookie(key string) (*http.Cookie, error) {
 	return ctx.Request.Cookie(key)
 }
 
+// File returns the named file provided in the request.
 func (ctx *Context) File(key string) (multipart.File, *multipart.FileHeader, error) {
 	return ctx.Request.File(key)
 }
 
+// Header returns the header value from the request by the given key.
 func (ctx *Context) Header(key string) string {
 	return ctx.Request.Header(key)
 }
 
+// MultiValuesHeader returns a string array value for the given key from the request header.
 func (ctx *Context) MultiValuesHeader(key string) []string {
 	return ctx.Request.MultiValuesHeader(key)
 }
 
+// Method returns the request method.
 func (ctx *Context) Method() string {
 	return ctx.Request.Method()
 }
 
+// Path returns the request path.
 func (ctx *Context) Path() string {
 	return ctx.Request.Path()
 }
 
+// Post returns the request post data.
 func (ctx *Context) Post() string {
 	return ctx.Request.Post()
 }
 
+// PostJSON returns the request post data as JSON object.
+func (ctx *Context) PostJSON() (interface{}, error) {
+	return ctx.Request.PostJSON()
+}
+
+// PostForm returns the request form data.
 func (ctx *Context) PostForm(key string) string {
 	return ctx.Request.PostForm(key)
 }
 
+// Query returns the query value from the request by the given key.
 func (ctx *Context) Query(key string) string {
 	return ctx.Request.Query(key)
 }
 
+// QueryDefault returns the query value from the request by the given key, and returns the
+// default value if the key is not found.
 func (ctx *Context) QueryDefault(key, defaultValue string) string {
 	val := ctx.Query(key)
 
@@ -157,10 +179,13 @@ func (ctx *Context) QueryDefault(key, defaultValue string) string {
 	return val
 }
 
+// MultiValuesQuery returns a string array value for the given key from the request query.
 func (ctx *Context) MultiValuesQuery(key string) []string {
 	return ctx.Request.MultiValuesQuery(key)
 }
 
+// MultiValuesQueryDefault returns a string array value for the given key from the request query
+// string, and returns the default values if the key is not exists.
 func (ctx *Context) MultiValuesQueryDefault(key string, defaultValues []string) []string {
 	val := ctx.MultiValuesQuery(key)
 
@@ -170,14 +195,19 @@ func (ctx *Context) MultiValuesQueryDefault(key string, defaultValues []string) 
 	return val
 }
 
+// Success writes the given data to the response body as JSON, and set the status code to 200 (OK).
 func (ctx *Context) Success(data interface{}) error {
 	return ctx.JSON(data, http.StatusOK)
 }
 
+// Fail writes the given data to the response body as JSON, and set the status code
+// to 400 (Bad Request).
 func (ctx *Context) Fail(data interface{}) error {
 	return ctx.JSON(data, http.StatusBadRequest)
 }
 
+// JSON stringifies and writes the given data to the response body, and set the content type
+// to "application/json".
 func (ctx *Context) JSON(data interface{}, statusCode ...int) error {
 	payload, err := json.Marshal(data)
 	if err != nil {
@@ -187,14 +217,20 @@ func (ctx *Context) JSON(data interface{}, statusCode ...int) error {
 	return ctx.send(payload, MIMETypeJSON, statusCode...)
 }
 
+// HTML writes the given data to the response body as HTML, and set the content type
+// to "text/html".
 func (ctx *Context) HTML(html string, statusCode ...int) error {
 	return ctx.send([]byte(html), MIMETypeHTML, statusCode...)
 }
 
+// String writes the given string to the response body, and sets the context type
+// to "text/plain".
 func (ctx *Context) String(data string, statusCode ...int) error {
 	return ctx.send([]byte(data), MIMETypeText, statusCode...)
 }
 
+// Redirect redirects the request to the given URL, it'll set status code to 302 (Found) as
+// default status code if it isn't set.
 func (ctx *Context) Redirect(url string, statusCode ...int) {
 	if url == "back" {
 		url = ctx.Request.Header(HTTPHeaderReferrer)
@@ -210,10 +246,27 @@ func (ctx *Context) Redirect(url string, statusCode ...int) {
 	ctx.Response.SetHeader(HTTPHeaderLocation, url)
 }
 
+// AddCookies adds one or more given cookies to the response.
 func (ctx *Context) AddCookies(cookies ...*http.Cookie) {
 	ctx.Response.AddCookies(cookies...)
 }
 
+// AddHeader appends the given header pair to the response.
+func (ctx *Context) AddHeader(key, val string) {
+	ctx.Response.AddHeader(key, val)
+}
+
+// SetHeader sets the value of the given header key.
 func (ctx *Context) SetHeader(key, val string) {
 	ctx.Response.SetHeader(key, val)
+}
+
+// SetStatusCode sets the status code of the response.
+func (ctx *Context) SetStatusCode(code int) error {
+	return ctx.Response.SetStatusCode(code)
+}
+
+// SetContentType sets the content type of the response.
+func (ctx *Context) SetContentType(contentType string) {
+	ctx.Response.SetContentType(contentType)
 }
